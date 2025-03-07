66b86967ef736083522b69b7baa159959a444baf
SENNA2077 <talisonsenna747@gmail.com>
2025-03-06 21:56:28 -0300

    Aula03

---
+++ aulas/aula03/App.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white", flexDirection: "row" }}>
+      <View style= {{flex: 1, flexDirection:"space-evenly", align: "itens"}}></View>
+    
+    </View>
+  );
+}
+
+export default App;
