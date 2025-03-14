7fcffc1344eae81e1981b76ad836a5d698a94187
SENNA2077 <talisonsenna747@gmail.com>
2025-03-13 21:40:46 -0300

    PRATICA 03

---
+++ aulas/praticas/pratica03/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ListScreen from "./screens/ListScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+function App() {
+return <ProfileScreen/>;
+
+};
+
+
+export default App;
