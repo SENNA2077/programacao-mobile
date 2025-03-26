190bde58cd257e1f85c6692efae3aaccd64b4728
SENNA2077 <talisonsenna747@gmail.com>
2025-03-25 21:06:23 -0300

    Pratica 04 ok

---
+++ aulas/praticas/pratica04/App.js
@@ -0,0 +1,11 @@
+import GalleryScreen from "./GalleryScreen";
+import RegisterScreen from "./RegisterScreen";
+import ContactScreen from "./screens/ContactScreen";
+import DetailScreen from "./screens/DetailScreen";
+
+function App() {
+
+  return <ContactScreen/>;
+}
+
+export default App;
